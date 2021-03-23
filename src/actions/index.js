0c90c2e053f7e0a069120be1fa0bda72b7a4a0b3
bryanjug <bryandjug@gmail.com>
2021-03-23 00:13:04 -0700

    refactored single component into streamCreate and streamForm

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM } from "./types";
 
 export const signIn = (userId) => {
@@ -21,7 +22,7 @@ export const createStream = formValues => async (dispatch, getState) => {
 
 	dispatch({type: CREATE_STREAM, payload: response.data});
 
-	
+	history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
